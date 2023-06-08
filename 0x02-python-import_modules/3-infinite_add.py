59024da3bb72ddc4da680b74605477987070b863
kerzers <ahra.ahlame@gmail.com>
2023-06-08 07:55:59 -0700

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv
+result = 0
+if __name__ == "__main__":
+    for i in range(1, len(argv)):
+        result += int(argv[i])
+    print("{}".format(result))
