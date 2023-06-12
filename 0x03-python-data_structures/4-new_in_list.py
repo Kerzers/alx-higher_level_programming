43bbbe102aefa89ee93823dfa7bf108c239f655f
kerzers <ahra.ahlame@gmail.com>
2023-06-12 04:05:58 -0700

    fix pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 without modifing the original"""
 
 
-def new_in_list(my_list, idx, element):    
+def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
     cpy_list = my_list.copy()
