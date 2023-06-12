f448abd9295c4623f06ebb48c24e679787dec952
kerzers <ahra.ahlame@gmail.com>
2023-06-12 04:03:25 -0700

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that replaces an element of a list at a specific position
+without modifing the original"""
+
+
+def new_in_list(my_list, idx, element):    
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    cpy_list = my_list.copy()
+    cpy_list.pop(idx)
+    cpy_list.insert(idx, element)
+    return (cpy_list)
