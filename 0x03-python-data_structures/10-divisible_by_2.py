998e75a96d39783ce05a57f0bccd0332b8ceb81a
kerzers <ahra.ahlame@gmail.com>
2023-06-12 11:41:12 -0700

    Divisible only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that finds all multiples of 2 in a list"""
+
+
+def divisible_by_2(my_list=[]):
+    divisible = [True if elmt % 2 == 0 else False for elmt in my_list]
+    print(divisible)
+    return divisible
