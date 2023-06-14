dd699f8a84a8835e7aecff6f1e6620e519ac1abc
kerzers <ahra.ahlame@gmail.com>
2023-06-14 13:14:49 -0700

    Fix case: is not a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
     roman_numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                      'D': 500, 'M': 1000}
     converted_int = 0
