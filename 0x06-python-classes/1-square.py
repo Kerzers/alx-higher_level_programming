bec4dd10a841661c0ce35d0eca8fbe9ff969eb08
kerzers <ahra.ahlame@gmail.com>
2023-06-27 07:32:22 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+
+class Square:
+    """Class Square that defines a square by: size."""
+
+    def __init__(self, size):
+        """Initilize a class square by size
+
+        Args:
+        size (int): size of square
+        """
+        self.__size = size
