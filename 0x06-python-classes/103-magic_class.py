4affe1431f463aa3c33e04afed7dd471540c5b35
kerzers <ahra.ahlame@gmail.com>
2023-06-29 14:29:03 -0700

    MagicClass

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module defines magic class"""
+import math
+
+
+class MagicClass:
+    """ Class MgicClass defined by an attribute:
+    raduis: radius of a circle
+    """
+    def __init__(self, radius=0):
+        """Initilize the object of MagicClass"""
+        if not type(radius, int) or type(radius, float):
+            raise TypeError('radius must be a number')
+        self.radius = radius
+
+    def area(self):
+        """ Calculates the area of a circle"""
+        return pi * (self.radius ** 2)
+
+    def circumference(self):
+        """Calculates circumference of a cercle"""
+        return 2 * pi * self.raduis
