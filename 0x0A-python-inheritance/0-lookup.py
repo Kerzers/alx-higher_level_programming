1f4aab108d66b741d47a77df36cb29b65282a908
kerzers <ahra.ahlame@gmail.com>
2023-07-10 10:32:05 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module that defines a function lookup"""
+
+
+def lookup(obj):
+    """ function that returns the list of available attributes
+    and methods of an object"""
+    return dir(obj)
