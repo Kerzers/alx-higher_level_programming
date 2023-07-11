4da424a2630a796f90b776043273910579992071
kerzers <ahra.ahlame@gmail.com>
2023-07-11 13:15:18 -0700

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module that defines class: Student"""
+
+
+class Student:
+    """a class Student that defines a student by:
+    Public instance attributes:
+    first_name (str)
+    last_name (str)
+    age (int)
+    """
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialization of an object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student"""
+        return vars(self)
