e938a5152b4995f888f2220aba3504cd08bf2f48
kerzers <ahra.ahlame@gmail.com>
2023-07-11 11:01:43 -0700

    load from jason file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module defines function: load_from_json_file"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file”"""
+
+    with open(filename, mode="r", encoding="utf_8") as f:
+        json.load(my_obj, f)
