5e68140eb47f8e17445d84a6c03e718377589475
kerzers <ahra.ahlame@gmail.com>
2023-07-13 06:06:13 -0700

    task 1

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,12 @@
+This project is about using all these concepts:
+******
+Import
+Exceptions
+Class
+Private attribute
+Getter/Setter
+Class method
+Static method
+Inheritance
+Unittest
+Read/Write file
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" this module defines the class: Base"""
+
+
+class Base:
+    """ this is the class Base defined by:
+    attribute:
+    nb_objects (int): private class attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor:
+        if id is not None, it assigns the id with its argument value
+        otherwise, increment __nb_objects and assign the new value to id
+        args:
+        id (int): the id of the object
+        """
+        if id is not None:
+            self.id = id
+        else:
+            self.id = __nb_objects
