a11008ba2d3967c6428b54683c14db2d97baf922
kerzers <ahra.ahlame@gmail.com>
2023-08-16 12:46:16 -0700

    task 8&9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- can be found in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
-ON cities.id = states.id
+	ON cities.id = states.id
 ORDER BY cities.id ASC;
