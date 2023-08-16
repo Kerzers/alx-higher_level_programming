5e9605d9b2f49e3e8ace7c5827c69625210dc483
kerzers <ahra.ahlame@gmail.com>
2023-08-16 05:34:36 -0700

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,5 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+-- user_0d_1 should have all privileges on your MySQL server
+GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost';
