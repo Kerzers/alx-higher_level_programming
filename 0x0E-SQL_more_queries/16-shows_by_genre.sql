5949601bb0873eb1aba28a1e25cedf2e8dfbd84b
kerzers <ahra.ahlame@gmail.com>
2023-08-17 06:17:28 -0700

    task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows contained in hbtn_0d_tvshows.
+SELECT t.title, tvg.name
+FROM tv_shows AS `t`
+LEFT JOIN tv_show_genres AS `tg`
+ON t.id = tg.show_id
+	LEFT JOIN tv_genres AS tvg
+	ON tg.genre_id = tvg.id
+ORDER BY t.title, tvg.name;
